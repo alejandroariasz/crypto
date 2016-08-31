00907b0051b3e23da5a068a78e7f0eec3ba10e16
Hugo Alejandro Arias <Hugo Alejandro Arias>
2016-08-30 23:41:01 -0500

    accesing to Google EP

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+var apiKey = 'crypto-142003';
+var clientId = '1097194985922-rvgtvh1s73i88h9f3ojfb2dvhbcorl90.apps.googleusercontent.com';
+var scopes = 'profile';
+
+function init() {
+	
+	gapi.client.setApiKey(apiKey);
+	var rootpath = "https://" + "crypto-142003.appspot.com" + "/_ah/api";	
+	gapi.client.load('cryptoAPI', 'v1', loadCallback, rootpath);
+}
+
+function loadCallback () {	
+	alert("hola");
+}
