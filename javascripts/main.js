4d164dba4ca350c60dd9189740743300e1dfd1c7
Hugo Alejandro Arias <Hugo Alejandro Arias>
2016-09-04 16:59:39 -0500

    fix

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,11 @@
-var apiKey = 'crypto-142003';
+var apiKey = 'crypto-142421';
 var clientId = '1097194985922-rvgtvh1s73i88h9f3ojfb2dvhbcorl90.apps.googleusercontent.com';
 var scopes = 'profile';
 
 function init() {
 	
 	gapi.client.setApiKey(apiKey);
-	var rootpath = 'https://' + 'crypto-142003.appspot.com' + '/_ah/api';	
+	var rootpath = 'https://' +  apiKey + '.appspot.com' + '/_ah/api';	
 	gapi.client.load('cryptoAPI', 'v2', function(){
 		var plainText = 'THEBOYWALKSINTHE';
 		var request = gapi.client.cryptoAPI.getN({'n':plainText});
